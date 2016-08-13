295a7010014df0fdbe75e7b177adb6336bafaf93
Rei Lin <temisrei@gmail.com>
2016-08-13 11:10:25 +0800

    06 練習 Angular 2 繫結方法：屬性繫結 ( Property Binding )

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title = 'Hello World'
+  titleurl = 'http://blog.miniasp.com/'
+  subtitle = '記載著 <strong>Will</strong> 在網路世界的學習心得與技術分享'
+
+
   constructor() { }
 
   ngOnInit() {
